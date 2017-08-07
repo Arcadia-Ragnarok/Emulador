b20b82d9d40617f541952dfefa720b551f3de348
Spell Master <spellmastergames@gmail.com>
2017-08-07 20:17:48 -0300

    Quest de classe para arqueiro

---
+++ NpcScript/Outros/Funcoes.c
@@ -0,0 +1,28 @@
+/*-----------------------------------------------------------------*\
+|              ____                     _                           |
+|             /    |                   | |_                         |
+|            /     |_ __ ____  __ _  __| |_  __ _                   |
+|           /  /|  | '__/  __|/ _` |/ _  | |/ _` |                  |
+|          /  __   | | |  |__| (_| | (_| | | (_| |                  |
+|         /  /  |  |_|  \____|\__,_|\__,_|_|\__,_|                  |
+|        /__/   |__|  [ Ragnarok Emulator ]                         |
+|                                                                   |
++-------------------------------------------------------------------+
+| - Desenvolvido por: Spell Master                                  |
+| - Nota: Funções de auxilio a Scripts e Itens.                     |
+| - Observação:                                                     |
+|   * Estas funções visam diminuir a quantidade de linhas de        |
+|     Scripts.                                                      |
+\*-----------------------------------------------------------------*/
+
+// ------------------------------------------------------------------
+// - [ ClearJobVar ] -
+// - Localiza variáveis atribuidas ao personagem durante as quests
+// de mudança de classe e Remove.
+// * Semelhante a "F_ClearJobVar" padrão dos emuladores.
+// ------------------------------------------------------------------
+
+function	script	ClearJobVar	{
+	if (archerquest) { archerquest = 0; }
+	return;
+}
