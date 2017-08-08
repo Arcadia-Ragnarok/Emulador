03c2eed410c0867f7aad49a42c48b20ee9949487
Spell Master <spellmastergames@gmail.com>
2017-08-08 14:06:19 -0300

    Update mapflag

--- NpcScript/Outros/Definicao_de_Mapa.c
+++ NpcScript/Outros/Definicao_de_Mapa.c
@@ -22,7 +22,18 @@ bat_room	mapflag	noteleport
 bat_room	mapflag	nowarpto
 // Tierra
 bat_a01	mapflag	battleground
+bat_a01	mapflag	zone Battlegrounds
 // Flavius
 bat_b01	mapflag	battleground	2
+bat_b01	mapflag	zone Battlegrounds
 // Kvm
 bat_c01	mapflag	battleground	2
+bat_c01	mapflag	zone Battlegrounds
+
+//-------------------------------------------------------------------
+// - [Mapas de Quest] 
+//-------------------------------------------------------------------
+new_1-1	mapflag	zone	JobQuest
+new_1-2	mapflag	zone	JobQuest
+new_1-3	mapflag	zone	JobQuest
+job_sword1	mapflag	zone	JobQuest
