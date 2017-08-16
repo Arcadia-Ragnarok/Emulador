09473c72847a8e3346a16cdffce295698ece498d
Spell Master <spellmastergames@gmail.com>
2017-08-16 03:27:17 -0300

    Nightenabled update

--- NpcScript/MapFlag/BattleGround.c
+++ NpcScript/MapFlag/BattleGround.c
@@ -1,8 +1,20 @@
+/*-----------------------------------------------------------------*\
+|              ____                     _                           |
+|             /    |                   | |_                         |
+|            /     |_ __ ____  __ _  __| |_  __ _                   |
+|           /  /|  | '__/  __|/ _` |/ _  | |/ _` |                  |
+|          /  __   | | |  |__| (_| | (_| | | (_| |                  |
+|         /  /  |  |_|  \____|\__,_|\__,_|_|\__,_|                  |
+|        /__/   |__|  [ Ragnarok Emulator ]                         |
+|                                                                   |
++-------------------------------------------------------------------+
+| - Nota: Maspas para definir Batalha Campal                        |
+\*-----------------------------------------------------------------*/
 
-bat_c01	mapflag	battleground	2
-bat_c02	mapflag	battleground	2
-bat_c03	mapflag	battleground	2
-bat_b01	mapflag	battleground	2
-bat_b02	mapflag	battleground
 bat_a01	mapflag	battleground
 bat_a02	mapflag	battleground
+bat_b01	mapflag	battleground	2
+bat_b02	mapflag	battleground
+bat_c03	mapflag	battleground	2
+bat_c01	mapflag	battleground	2
+bat_c02	mapflag	battleground	2
--- NpcScript/MapFlag/GvG.c
+++ NpcScript/MapFlag/GvG.c
@@ -1,3 +1,21 @@
+/*-----------------------------------------------------------------*\
+|              ____                     _                           |
+|             /    |                   | |_                         |
+|            /     |_ __ ____  __ _  __| |_  __ _                   |
+|           /  /|  | '__/  __|/ _` |/ _  | |/ _` |                  |
+|          /  __   | | |  |__| (_| | (_| | | (_| |                  |
+|         /  /  |  |_|  \____|\__,_|\__,_|_|\__,_|                  |
+|        /__/   |__|  [ Ragnarok Emulator ]                         |
+|                                                                   |
++-------------------------------------------------------------------+
+| - Nota: Castelos da Guerra de Imperio                             |
+\*-----------------------------------------------------------------*/
+
+prtg_cas01	mapflag	gvg_castle
+prtg_cas02	mapflag	gvg_castle
+prtg_cas03	mapflag	gvg_castle
+prtg_cas04	mapflag	gvg_castle
+prtg_cas05	mapflag	gvg_castle
 
 aldeg_cas01	mapflag	gvg_castle
 aldeg_cas02	mapflag	gvg_castle
@@ -17,11 +35,10 @@ payg_cas03	mapflag	gvg_castle
 payg_cas04	mapflag	gvg_castle
 payg_cas05	mapflag	gvg_castle
 
-prtg_cas01	mapflag	gvg_castle
-prtg_cas02	mapflag	gvg_castle
-prtg_cas03	mapflag	gvg_castle
-prtg_cas04	mapflag	gvg_castle
-prtg_cas05	mapflag	gvg_castle
+gld_dun01	mapflag	gvg_dungeon
+gld_dun02	mapflag	gvg_dungeon
+gld_dun03	mapflag	gvg_dungeon
+gld_dun04	mapflag	gvg_dungeon
 
 schg_cas01	mapflag	gvg_castle
 schg_cas02	mapflag	gvg_castle
@@ -34,11 +51,5 @@ arug_cas02	mapflag	gvg_castle
 arug_cas03	mapflag	gvg_castle
 arug_cas04	mapflag	gvg_castle
 arug_cas05	mapflag	gvg_castle
-
-gld_dun01	mapflag	gvg_dungeon
-gld_dun02	mapflag	gvg_dungeon
-gld_dun03	mapflag	gvg_dungeon
-gld_dun04	mapflag	gvg_dungeon
-
 arug_dun01	mapflag	gvg_dungeon
 schg_dun01	mapflag	gvg_dungeon
