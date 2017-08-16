34d52eb5336a1ffc89c275cf158e646a4a41e027
Spell Master <spellmastergames@gmail.com>
2017-08-16 05:04:38 -0300

    Conclusão da revisão dos MapFlag's

--- NpcScript/MapFlag/NoPenalty.c
+++ NpcScript/MapFlag/NoPenalty.c
@@ -11,11 +11,11 @@
 | - Nota: Mapas onde a perde de experiência é desabilitada          |
 \*-----------------------------------------------------------------*/
 
+bat_a01	mapflag	nopenalty
+bat_a02	mapflag	nopenalty
+bat_b01	mapflag	nopenalty
+bat_b02	mapflag	nopenalty
 bat_c01	mapflag	nopenalty
 bat_c02	mapflag	nopenalty
 bat_c03	mapflag	nopenalty
 bat_room	mapflag	nopenalty
-bat_b01	mapflag	nopenalty
-bat_b02	mapflag	nopenalty
-bat_a01	mapflag	nopenalty
-bat_a02	mapflag	nopenalty
