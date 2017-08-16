5a7427e7c49780d0d84928851c5104b0b62c44b2
Spell Master <spellmastergames@gmail.com>
2017-08-16 03:47:42 -0300

    Nobranch update

--- NpcScript/MapFlag/NoBranch.c
+++ NpcScript/MapFlag/NoBranch.c
@@ -1,62 +1,38 @@
+/*-----------------------------------------------------------------*\
+|              ____                     _                           |
+|             /    |                   | |_                         |
+|            /     |_ __ ____  __ _  __| |_  __ _                   |
+|           /  /|  | '__/  __|/ _` |/ _  | |/ _` |                  |
+|          /  __   | | |  |__| (_| | (_| | | (_| |                  |
+|         /  /  |  |_|  \____|\__,_|\__,_|_|\__,_|                  |
+|        /__/   |__|  [ Ragnarok Emulator ]                         |
+|                                                                   |
++-------------------------------------------------------------------+
+| - Nota: Mapas onde não é permitido usar Galho Seco/               |
+          Galho Sangrento e itens de invocação de monstros agrecivos|
+\*-----------------------------------------------------------------*/
 
-alb_ship	mapflag	nobranch
-
-ama_test	mapflag	nobranch
+prt_are01	mapflag	nobranch
+prt_monk	mapflag	nobranch
+izlu2dun	mapflag	nobranch
+alb2trea	mapflag	nobranch
 in_orcs01	mapflag	nobranch
-new_1-1	mapflag	nobranch
-new_1-2	mapflag	nobranch
-new_1-3	mapflag	nobranch
-
-que_sign01	mapflag	nobranch
-airport	mapflag	nobranch
-airplane	mapflag	nobranch
-airplane_01	mapflag	nobranch
-yuno_pre	mapflag	nobranch
-y_airport	mapflag	nobranch
-lhz_airport	mapflag	nobranch
-
-lhz_que01	mapflag	nobranch
-lhz_cube	mapflag	nobranch
-
-ra_temin	mapflag	nobranch
-ra_temple	mapflag	nobranch
-ra_temsky	mapflag	nobranch
-thor_camp	mapflag	nobranch
-
-job_sword1	mapflag	nobranch
-job_thief1	mapflag	nobranch
-job_knt	mapflag	nobranch
-job_prist	mapflag	nobranch
-job_wiz	mapflag	nobranch
-job_hunte	mapflag	nobranch
 in_hunter	mapflag	nobranch
-in_moc_16	mapflag	nobranch
-alde_alche	mapflag	nobranch
-job_cru	mapflag	nobranch
-job_duncer	mapflag	nobranch
-job_monk	mapflag	nobranch
-monk_test	mapflag	nobranch
+moc_prydb1	mapflag	nobranch
+yuno_pre	mapflag	nobranch
 monk_in	mapflag	nobranch
+in_moc_16	mapflag	nobranch
 in_rogue	mapflag	nobranch
 job_sage	mapflag	nobranch
-que_ng	mapflag	nobranch
-job_soul	mapflag	nobranch
-job_star	mapflag	nobranch
-
-que_god01	mapflag	nobranch
-que_god02	mapflag	nobranch
-
-kh_mansion	mapflag	nobranch
-kh_rossi	mapflag	nobranch
-kh_school	mapflag	nobranch
-kh_vila	mapflag	nobranch
-kh_kiehl01	mapflag	nobranch
-kh_kiehl02	mapflag	nobranch
-que_rachel	mapflag	nobranch
-que_san04	mapflag	nobranch
-que_thor	mapflag	nobranch
-
+monk_test	mapflag	nobranch
+prt_gld	mapflag	nobranch
 alde_gld	mapflag	nobranch
+pay_gld	mapflag	nobranch
+payg_cas01	mapflag	nobranch
+payg_cas02	mapflag	nobranch
+payg_cas03	mapflag	nobranch
+payg_cas04	mapflag	nobranch
+payg_cas05	mapflag	nobranch
 aldeg_cas01	mapflag	nobranch
 aldeg_cas02	mapflag	nobranch
 aldeg_cas03	mapflag	nobranch
@@ -67,18 +43,81 @@ gefg_cas02	mapflag	nobranch
 gefg_cas03	mapflag	nobranch
 gefg_cas04	mapflag	nobranch
 gefg_cas05	mapflag	nobranch
-pay_gld	mapflag	nobranch
-payg_cas01	mapflag	nobranch
-payg_cas02	mapflag	nobranch
-payg_cas03	mapflag	nobranch
-payg_cas04	mapflag	nobranch
-payg_cas05	mapflag	nobranch
-prt_gld	mapflag	nobranch
 prtg_cas01	mapflag	nobranch
 prtg_cas02	mapflag	nobranch
 prtg_cas03	mapflag	nobranch
 prtg_cas04	mapflag	nobranch
 prtg_cas05	mapflag	nobranch
+gld_dun01	mapflag	nobranch
+gld_dun02	mapflag	nobranch
+gld_dun03	mapflag	nobranch
+gld_dun04	mapflag	nobranch
+ama_test	mapflag	nobranch
+gon_fild01	mapflag	nobranch
+yggdrasil01	mapflag	nobranch
+valkyrie	mapflag	nobranch
+himinn	mapflag	nobranch
+lhz_cube	mapflag	nobranch
+lhz_que01	mapflag	nobranch
+airplane	mapflag	nobranch
+airplane_01	mapflag	nobranch
+airport	mapflag	nobranch
+y_airport	mapflag	nobranch
+lhz_airport	mapflag	nobranch
+thana_step	mapflag	nobranch
+thana_boss	mapflag	nobranch
+tha_scene01	mapflag	nobranch
+tha_t01	mapflag	nobranch
+tha_t02	mapflag	nobranch
+tha_t03	mapflag	nobranch
+tha_t04	mapflag	nobranch
+tha_t07	mapflag	nobranch
+tha_t05	mapflag	nobranch
+tha_t06	mapflag	nobranch
+tha_t08	mapflag	nobranch
+tha_t09	mapflag	nobranch
+tha_t10	mapflag	nobranch
+tha_t11	mapflag	nobranch
+tha_t12	mapflag	nobranch
+alde_alche	mapflag	nobranch
+que_ng	mapflag	nobranch
+jupe_area1	mapflag	nobranch
+jupe_area2	mapflag	nobranch
+jupe_core	mapflag	nobranch
+jupe_ele	mapflag	nobranch
+jupe_ele_r	mapflag	nobranch
+jupe_gate	mapflag	nobranch
+jupe_cave	mapflag	nobranch
+gefenia01	mapflag	nobranch
+gefenia02	mapflag	nobranch
+gefenia03	mapflag	nobranch
+gefenia04	mapflag	nobranch
+que_god01	mapflag	nobranch
+que_god02	mapflag	nobranch
+que_sign01	mapflag	nobranch
+que_sign02	mapflag	nobranch
+kh_rossi	mapflag	nobranch
+kh_school	mapflag	nobranch
+kh_vila	mapflag	nobranch
+kh_mansion	mapflag	nobranch
+kh_kiehl01	mapflag	nobranch
+kh_kiehl02	mapflag	nobranch
+kh_dun01	mapflag	nobranch
+kh_dun02	mapflag	nobranch
+ra_temin	mapflag	nobranch
+ra_temple	mapflag	nobranch
+ra_temsky	mapflag	nobranch
+que_rachel	mapflag	nobranch
+que_san04	mapflag	nobranch
+que_thor	mapflag	nobranch
+thor_camp	mapflag	nobranch
+z_agit	mapflag	nobranch
+que_temsky	mapflag	nobranch
+nameless_i	mapflag	nobranch
+nameless_n	mapflag	nobranch
+nameless_in	mapflag	nobranch
+mosk_que	mapflag	nobranch
+e_tower	mapflag	nobranch
 sch_gld	mapflag	nobranch
 schg_cas01	mapflag	nobranch
 schg_cas02	mapflag	nobranch
@@ -91,60 +130,20 @@ arug_cas02	mapflag	nobranch
 arug_cas03	mapflag	nobranch
 arug_cas04	mapflag	nobranch
 arug_cas05	mapflag	nobranch
-
-prt_are01	mapflag	nobranch
-
-e_tower	mapflag	nobranch
-
-mid_campin	mapflag	nobranch
-man_fild01	mapflag	nobranch
-moc_fild22b	mapflag	nobranch
-que_dan01	mapflag	nobranch
-que_dan02	mapflag	nobranch
-
-bat_c01	mapflag	nobranch
-bat_c02	mapflag	nobranch
-bat_c03	mapflag	nobranch
 bat_room	mapflag	nobranch
 bat_b01	mapflag	nobranch
 bat_b02	mapflag	nobranch
 bat_a01	mapflag	nobranch
 bat_a02	mapflag	nobranch
-
-man_fild02	mapflag	nobranch
-
+bat_c01	mapflag	nobranch
+bat_c02	mapflag	nobranch
+bat_c03	mapflag	nobranch
+mid_campin	mapflag	nobranch
+moc_fild22b	mapflag	nobranch
 moc_para01	mapflag	nobranch
-job3_arch01	mapflag	nobranch
-job3_arch02	mapflag	nobranch
-job3_guil01	mapflag	nobranch
-job3_guil02	mapflag	nobranch
-job3_guil03	mapflag	nobranch
-job3_rang01	mapflag	nobranch
-job3_rang02	mapflag	nobranch
-job3_rune01	mapflag	nobranch
-job3_rune02	mapflag	nobranch
-job3_war01	mapflag	nobranch
-jupe_core2	mapflag	nobranch
-
-dicastes01	mapflag	nobranch
-dicastes02	mapflag	nobranch
-dic_in01	mapflag	nobranch
-s_atelier	mapflag	nobranch
-
-mora	mapflag	nobranch
-dewata	mapflag	nobranch
-dew_in01	mapflag	nobranch
-
-malangdo	mapflag	nobranch
-mal_in01	mapflag	nobranch
-mal_in02	mapflag	nobranch
-malaya	mapflag	nobranch
+que_dan01	mapflag	nobranch
+que_dan02	mapflag	nobranch
+que_lhz	mapflag	nobranch
 ma_scene01	mapflag	nobranch
-ma_in01	mapflag	nobranch
-
-eclage	mapflag	nobranch
-ecl_in01	mapflag	nobranch
-ecl_in02	mapflag	nobranch
-ecl_in03	mapflag	nobranch
-ecl_in04	mapflag	nobranch
 ecl_hub01	mapflag	nobranch
+que_avan01	mapflag	nobranch
