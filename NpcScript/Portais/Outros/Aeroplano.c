c04361724ac9ed713b36a48373bad77d818e537c
Spell-Master <spellmastergames@gmail.com>
2017-08-30 02:14:19 -0300

    Correção de portais nos aeroportos e aeroplano

--- NpcScript/Portais/Outros/Aeroplano.c
+++ NpcScript/Portais/Outros/Aeroplano.c
@@ -8,50 +8,48 @@
 |        /__/   |__|  [ Ragnarok Emulator ]                         |
 |                                                                   |
 +-------------------------------------------------------------------+
-| - Script: eAthena/ rAthena/ Hercules (Emuladores)                 |
-| - Versão: Spell Master                                            |
 | - Nota: Potais nos Aeroportos e Aeroplano                         |
 \*-----------------------------------------------------------------*/
 
 // ------------------------------------------------------------------
 // - [ Aeroportos ] -
 // ------------------------------------------------------------------
-airport,142,61,0	warp	ein_ap02a	1,1,einbroch,62,246
-airport,124,13,0	warp	ein_ap03	1,1,airport,19,21
-airport,19,18,0	warp	ein_ap03a	1,1,airport,122,16
-airport,161,13,0	warp	ein_ap04	1,1,airport,47,21
-airport,47,18,0	warp	ein_ap04a	1,1,airport,163,16
-airport,143,14,0	warp	ein_ap05	1,1,einbroch,64,204
+airport,142,61,0	warp	airportwarp_1	1,1,einbroch,62,246
+airport,124,13,0	warp	airportwarp_2	1,1,airport,19,21
+airport,161,13,0	warp	airportwarp_3	1,1,airport,47,21
+airport,143,14,0	warp	airportwarp_4	1,1,einbroch,64,204
+airport,19,18,0	warp	airportwarp_5	1,1,airport,122,16
+airport,47,18,0	warp	airportwarp_6	1,1,airport,163,16
 
 // ------------------------------------------------------------------
-lhz_airport,125,14,0	warp	lairp1	1,1,lhz_airport,19,20
-lhz_airport,19,18,0	warp	lairp1a	1,1,lhz_airport,123,14
-lhz_airport,160,14,0	warp	lairp2	1,1,lhz_airport,48,20
-lhz_airport,48,18,0	warp	lairp2a	1,1,lhz_airport,162,14
-lhz_airport,143,13,0	warp	lairp3a	1,1,lighthalzen,265,76
-lhz_airport,142,60,0	warp	lairp4	1,1,lighthalzen,296,76
+lhz_airport,142,60,0	warp	lhzairportwarp_1	1,1,lighthalzen,296,76
+lhz_airport,125,13,0	warp	lhzairportwarp_2	1,1,lhz_airport,19,23
+lhz_airport,160,13,0	warp	lhzairportwarp_3	1,1,lhz_airport,47,23
+lhz_airport,142,13,0	warp	lhzairportwarp_4	1,1,lighthalzen,262,75
+lhz_airport,19,18,0	warp	lhzairportwarp_5	1,1,lhz_airport,121,16
+lhz_airport,47,18,0	warp	lhzairportwarp_6	1,1,lhz_airport,163,16
 
 // ------------------------------------------------------------------
-y_airport,125,14,0	warp	jairp1	1,1,y_airport,19,20
-y_airport,19,18,0	warp	jairp1a	1,1,y_airport,123,14
-y_airport,160,14,0	warp	jairp2	1,1,y_airport,48,20
-y_airport,48,18,0	warp	jairp2a	1,1,y_airport,162,14
-y_airport,143,16,0	warp	jairp3a	1,1,yuno,52,207
+y_airport,125,14,0	warp	yairportwarp_1	1,1,y_airport,19,20
+y_airport,19,18,0	warp	yairportwarp_2	1,1,y_airport,123,14
+y_airport,160,14,0	warp	yairportwarp_3	1,1,y_airport,48,20
+y_airport,48,18,0	warp	yairportwarp_4	1,1,y_airport,162,14
+y_airport,143,16,0	warp	yairportwarp_5	1,1,yuno,52,207
 
 // ------------------------------------------------------------------
 // - [ Aeroplanos ] -
 // ------------------------------------------------------------------
-airplane,254,54,0	warp	air_1wp01	1,1,airplane,91,67
-airplane,87,67,0	warp	air_1wp01a	1,1,airplane,250,54
-airplane,208,53,0	warp	air_1wp02	1,1,airplane,239,160
-airplane,245,160,0	warp	air_1wp02a	1,1,airplane,214,54
+airplane,87,67,0	warp	airplanewarp_1	1,1,airplane,250,54
+airplane,254,54,0	warp	airplanewarp_2	1,1,airplane,91,67
+airplane,208,53,0	warp	airplanewarp_3	1,1,airplane,239,160
+airplane,245,160,0	warp	airplanewarp_4	1,1,airplane,214,54
 
 // ------------------------------------------------------------------
-airplane_01,254,54,0	warp	air_2wp01	1,1,airplane_01,91,67
-airplane_01,87,67,0	warp	air_2wp01a	1,1,airplane_01,250,54
-airplane_01,208,53,0	warp	air_2wp02	1,1,airplane_01,239,160
-airplane_01,245,160,0	warp	air_2wp02a	1,1,airplane_01,214,54
-airplane_01,104,199,0	warp	air_2wp03	1,1,airplane_01,105,72
-airplane_01,103,72,0	warp	air_2wp03a	1,1,airplane_01,102,199
-airplane_01,104,176,0	warp	air_2wp04	1,1,airplane_01,105,52
-airplane_01,103,52,0	warp	air_2wp04a	1,1,airplane_01,102,176
+airplane_01,254,54,0	warp	airplanewarp_5	1,1,airplane_01,91,67
+airplane_01,87,67,0	warp	airplanewarp_6	1,1,airplane_01,250,54
+airplane_01,208,53,0	warp	airplanewarp_7	1,1,airplane_01,239,160
+airplane_01,245,160,0	warp	airplanewarp_8	1,1,airplane_01,214,54
+airplane_01,104,199,0	warp	airplanewarp_9	1,1,airplane_01,105,72
+airplane_01,103,72,0	warp	airplanewarp_10	1,1,airplane_01,102,199
+airplane_01,104,176,0	warp	airplanewarp_11	1,1,airplane_01,105,52
+airplane_01,103,52,0	warp	airplanewarp_12	1,1,airplane_01,102,176
